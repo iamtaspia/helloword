77f865fcf9c48f89772ddca6089836d507ef371c
iamtaspia <nusratjahantaspia000@gmail.com>
2023-01-22 23:33:52 +0600

    first commit

---
+++ Untitled1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b, sum;
+    printf("Enter the value of first number: ");
+    scanf("%d", &a);
+    printf("Enter the value of second number: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("Sum of two numbers: %d", sum);
+
+    return 0;
+}
